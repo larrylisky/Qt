73650de126f806c9ae0f7795f8e4fd5fb47234d6
Larry Li <larrylisky@gmail.com>
2017-05-12 10:44:13 -0500

    Able to see and rotate point cloud display

--- TOFViewer/TOFViewer/tofopenglwidget.cpp
+++ TOFViewer/TOFViewer/tofopenglwidget.cpp
@@ -10,7 +10,11 @@
 ToFOpenGLWidget::ToFOpenGLWidget(QWidget *parent)
     : QOpenGLWidget(parent)
 {
-
+    _leftButtonPressed = _rightButtonPressed = false;
+    _zoomFactor = 1.0;
+    _rotX = _rotY = 0.0;
+    _arrowSize = 0.25;
+    _axisLength = 5.0;
 }
 
 
@@ -19,61 +23,173 @@ ToFOpenGLWidget::~ToFOpenGLWidget()
 
 }
 
+
 void ToFOpenGLWidget::initializeGL()
 {
+    GLfloat light0_position[] = { 20.0, 20.0, -20.0, 0.0 };
+    GLfloat light1_position[] = { -20.0, 20.0, -20.0, 0.0 };
+
     glClearColor(0,0,0,1);
     glEnable(GL_DEPTH_TEST);
-    glEnable(GL_LIGHT0);
+    glLightfv(GL_LIGHT0, GL_POSITION, light0_position);
+    glLightfv(GL_LIGHT0, GL_POSITION, light1_position);
     glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHT1);
     glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
     glEnable(GL_COLOR_MATERIAL);
 }
+
 
 void ToFOpenGLWidget::resizeGL(int w, int h)
 {
     glViewport(0,0,w,h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(45, (float)w/h, 0.01, 100.0);
+    gluPerspective(45.0, (float)w/h, 0.01, 100.0);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(0,0,5,0,0,0,0,1,0);
+    gluLookAt(0,0,10,0,0,0,0,1,0);
 }
 
 void ToFOpenGLWidget::paintGL()
 {
-    glPushMatrix();
-
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_DEPTH_CLAMP);
+
+    glPushMatrix();
+    glRotatef(_rotX, 1,0,0);
+    glRotatef(_rotY+180, 0,1,0);
+
+    glPushMatrix();
+    glTranslatef(0.0,0.0,_zoomFactor);
+
+    glPushMatrix();
+    _drawAxis();
+    glPopMatrix();
 
     glBegin(GL_POINTS);
         glColor3f(1.0, 1.0, 1.0);
         glPointSize(2);
         for (int i=0; i < _frame.size(); i++)
         {
-            float x = _frame.points[i].x;
-            float y = _frame.points[i].y;
+            float x = -_frame.points[i].x;
+            float y = -_frame.points[i].y;
             float z = _frame.points[i].z;
             glVertex3f(x, y, z);
         }
     glEnd();
 
     glFlush();
-
     glPopMatrix();
-
+    glPopMatrix();
 }
 
 
 void ToFOpenGLWidget::drawPointCloud(XYZIPointCloudFrame *frame)
 {
-
-
     if (frame)
         _frame = *frame;
 
     paintGL();
     update();
+}
+
+
+void ToFOpenGLWidget::mousePressEvent(QMouseEvent *event)
+{
+    if (event->button() == 1)
+    {
+        _leftButtonPressed = true;
+        _startPos = event->pos();
+    }
+    else if (event->button() == 2)
+    {
+        _rightButtonPressed = true;
+        _startPos = event->pos();
+    }
 
 }
 
+
+void ToFOpenGLWidget::mouseReleaseEvent(QMouseEvent *event)
+{
+    if (event->button() == 1)
+    {
+        _leftButtonPressed = false;
+        _endPos = event->pos();
+    }
+    else if (event->button() == 2)
+    {
+        _rightButtonPressed = false;
+        _endPos = event->pos();
+    }
+}
+
+
+void ToFOpenGLWidget::mouseMoveEvent(QMouseEvent *event)
+{
+    if (_leftButtonPressed)
+    {
+        _rotY = event->pos().x() - _startPos.x();
+        _rotX = event->pos().y() - _startPos.y();
+    }
+}
+
+
+void ToFOpenGLWidget::wheelEvent(QWheelEvent *event)
+{
+    std::cout << "Wheel: " << event->delta() << std::endl;
+    _zoomFactor -= (float)event->delta()/1200.0;
+
+}
+
+void ToFOpenGLWidget::_drawAxis()
+{
+    // x axis- red
+    glColor3f(1.0,0.0,0.0);
+    glBegin(GL_LINES);
+        glVertex3f(-_axisLength, 0.0f, 0.0f);
+        glVertex3f(_axisLength, 0.0f, 0.0f);
+
+        // arrow
+        glVertex3f(_axisLength, 0.0f, 0.0f);
+        glVertex3f(_axisLength-_arrowSize, _arrowSize, 0.0f);
+
+        glVertex3f(_axisLength, 0.0f, 0.0f);
+        glVertex3f(_axisLength-_arrowSize, -_arrowSize, 0.0f);
+    glEnd();
+    glFlush();
+
+
+    // y axis - green
+    glColor3f(0.0,1.0,0.0);
+    glBegin(GL_LINES);
+        glVertex3f(0.0, -_axisLength, 0.0f);
+        glVertex3f(0.0, _axisLength, 0.0f);
+
+        // arrow
+        glVertex3f(0.0, _axisLength, 0.0f);
+        glVertex3f(_arrowSize, _axisLength-_arrowSize, 0.0f);
+
+        glVertex3f(0.0, _axisLength, 0.0f);
+        glVertex3f(-_arrowSize, _axisLength-_arrowSize, 0.0f);
+    glEnd();
+    glFlush();
+
+
+    // z axis - blue
+    glColor3f(0.0,0.0,1.0);
+    glBegin(GL_LINES);
+        glVertex3f(0.0, 0.0f ,-_axisLength );
+        glVertex3f(0.0, 0.0f ,_axisLength);
+
+        // arrow
+        glVertex3f(0.0, 0.0f ,_axisLength );
+        glVertex3f(0.0, _arrowSize ,_axisLength-_arrowSize );
+
+        glVertex3f(0.0, 0.0f ,_axisLength );
+        glVertex3f(0.0, -_arrowSize ,_axisLength-_arrowSize );
+    glEnd();
+    glFlush();
+}
--- TOFViewer/TOFViewer/tofopenglwidget.h
+++ TOFViewer/TOFViewer/tofopenglwidget.h
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <QMainWindow>
 #include <QOpenGLWidget>
+#include <QMouseEvent>
 #include <GL/glu.h>
 #include <GL/gl.h>
 #include <GL/glut.h>
@@ -30,8 +31,21 @@ protected:
     virtual void resizeGL(int w, int h);
     virtual void paintGL();
 
+    virtual void mousePressEvent(QMouseEvent *event);
+    virtual void mouseReleaseEvent(QMouseEvent *event);
+    virtual void mouseMoveEvent(QMouseEvent *event);
+    virtual void wheelEvent(QWheelEvent *event);
+
+    void _drawAxis();
+
 private:
     XYZIPointCloudFrame _frame;
+    bool _leftButtonPressed, _rightButtonPressed;
+    int _mouseX, _mouseY;
+    float _zoomFactor;
+    QPoint _startPos, _endPos;
+    float _rotX, _rotY;
+    float _arrowSize, _axisLength;
 };
 
 #endif // TOFOPENGLWIDGET_H
